bf6a7db052b70771297a0193f038e37092ab3ef5
c297009200 <hangyuchen@msm.com>
2019-05-31 11:38:24 +0800

    dependces

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './style.less'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className= "ok">
+          余杭 <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
